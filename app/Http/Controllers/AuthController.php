b58614f6f5191f69b479e9a372da103202925129
chamsedine-bouhouch <bouhouchchamseddine@gmail.com>
2023-05-25 22:17:40 +0200

    Login User API

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -43,4 +44,37 @@ class AuthController extends Controller
             return response()->json(['error' => 'Provide proper details']);
         }
     }
+    /**
+ * Login user and create token
+*
+* @param  [string] email
+* @param  [string] password
+* @param  [boolean] remember_me
+*/
+
+public function login(Request $request)
+{
+    $request->validate([
+    'email' => 'required|string|email',
+    'password' => 'required|string',
+    'remember_me' => 'boolean'
+    ]);
+
+    $credentials = request(['email','password']);
+    if(!Auth::attempt($credentials))
+    {
+    return response()->json([
+        'message' => 'Unauthorized'
+    ],401);
+    }
+
+    $user = $request->user();
+    $tokenResult = $user->createToken('Personal Access Token');
+    $token = $tokenResult->plainTextToken;
+
+    return response()->json([
+    'accessToken' =>$token,
+    'token_type' => 'Bearer',
+    ]);
+}
 }
